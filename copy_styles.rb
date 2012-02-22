91d75177edbe2676e82a4889aebb9a8c45871218
Josh Adams <josh@isotope11.com>
2012-02-22 13:49:37 -0600

    Update copy_styles script to do merges

--- copy_styles.rb
+++ copy_styles.rb
@@ -2,6 +2,8 @@
 `mkdir /tmp/jenkins-isotope-style`
 `cp *.css /tmp/jenkins-isotope-style`
 `git checkout gh-pages`
+`git fetch origin`
+`git merge origin/gh-pages`
 `cp /tmp/jenkins-isotope-style/*.css ./`
 `git add *.css`
 `git commit -m"Updated styles"`
